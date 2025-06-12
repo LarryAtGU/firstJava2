091ceb4337acf15fdd4ac3776b1bc7d3c84d0053
LarryAtGU <l.wen@griffith.edu.au>
2025-06-12 19:41:52 +1000

    add sub in Math

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        int x = 1,y=2;
-        System.out.println("Hello and welcome!");
+        int x = 2,y = 1;
+        System.out.println("1 + 2 =");
         System.out.println(Math.add(x,y));
+        System.out.println("2 - 1 = ");
+        System.out.println(Math.sub(x,y));
     }
 }
--- src/Math.java
+++ src/Math.java
@@ -2,4 +2,5 @@ public class Math {
     static public int add (int x, int y) {
         return x+y;
     }
+    static public int sub (int x, int y) {return x-y;}
 }
